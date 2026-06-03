0af0b914b58969d6b50ee45d4c3fd36c20a91515
EvilisGlenio <glenio.developer@gmail.com>
2026-06-03 10:35:11 -0300

    feat(web): redesign Mnemos Mundi player shell

--- apps/web/app/(metaverso)/mnemos-mundi/components/PortalLayout.tsx
+++ apps/web/app/(metaverso)/mnemos-mundi/components/PortalLayout.tsx
@@ -3,11 +3,5 @@
 import { MnemosMundiPlayer } from "./MnemosMundiPlayer";
 
 export function PortalLayout() {
-  return (
-    <div className="flex min-h-screen w-full overflow-hidden bg-[#020817] text-white">
-      <div className="w-full">
-        <MnemosMundiPlayer />
-      </div>
-    </div>
-  );
+  return <MnemosMundiPlayer />;
 }
